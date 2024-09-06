a2bee9a334263f2ce2abb6a8b938387f1e43d81f
DuocGauss <gu.martinezb@duocuc.cl>
2024-09-06 17:43:22 -0400

    Tercer commit

--- main.go
+++ main.go
@@ -1,1 +1,10 @@
-package main
+package main //El archivo principal de Go siempre debe importar main
+
+import (
+	"github.com/DuocGauss/Godesde0/variables"
+)
+
+// Función principal de main, se debe llamar main y no debe tener nada entre sus parentesis
+func main() {
+	variables.MuestroEnteros()
+}
--- variables/enteras.go
+++ variables/enteras.go
@@ -1,1 +1,15 @@
 package variables
+
+import "fmt" //Se debe importar este paquete para que Go tenga interacción con la terminal
+//y se le pueda mandar texto
+
+//Voy a crear una función global y definir 3 variables dentro de esa función
+func MuestroEnteros() {
+	var intcomun int
+	intde32 := int32(10)
+	intde64 := int64(100)
+
+	fmt.Println("Valor de intcomún =", intcomun)
+	fmt.Println("Valor de intde32 =", intde32)
+	fmt.Println("Valor de intde64 =", intde64)
+}
