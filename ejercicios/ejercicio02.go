6fd354ec04ffc9c9e95a8af7bb6f45084f63b2ac
DuocGauss <gu.martinezb@duocuc.cl>
2024-09-10 03:01:25 -0300

    Ejercicio 02

--- ejercicios/ejercicio02.go
+++ ejercicios/ejercicio02.go
@@ -7,20 +7,24 @@ import (
 	"strconv"
 )
 
+// Mostrar tablas de multiplicación a partir del input introducido del usuario, se multiplica hasta el 10
 func Multiplicar() {
+	var numero int
+	var err error
 	scanner := bufio.NewScanner(os.Stdin)
-
-	fmt.Println("Ingrse un número: ")
-	if scanner.Scan() {
-		numero, err := strconv.Atoi(scanner.Text())
-		for valor := 0; valor < 11; valor++ {
+	for {
+		fmt.Println("Ingrse un número: ")
+		if scanner.Scan() {
+			numero, err = strconv.Atoi(scanner.Text())
 			if err != nil {
-				fmt.Println("Hubo un error porfavor ingrese un número")
+				fmt.Println("Hubo un error, introduzca un valor númerico")
 				continue
 			} else {
-				fmt.Println(numero, "X", valor, "=", numero*valor)
+				for valor := 0; valor < 11; valor++ {
+					fmt.Println(numero, "X", valor, "=", numero*valor)
+				}
 			}
 		}
-
+		break
 	}
 }
